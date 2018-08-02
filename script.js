3d7f58ab5dcfeedbcded8a498a2a535d4e7891f6
Grace Gamble <grace.h.gamble@gmail.com>
2018-08-02 11:28:27 -0600

    The Prototype Chain in the Console

--- script.js
+++ script.js
@@ -15,25 +15,56 @@ Function Constructors
 
 // "Blueprint," or function constructor
 // First, the function is created, then it is called when we make a new one
+// var Person = function (name, yearOfBirth, job) {
+//       this.name = name;
+//       this.yearOfBirth = yearOfBirth;
+//       this.job = job;
+// }
+//
+// Person.prototype.calculateAge = function () {
+//       console.log(2018 - this.yearOfBirth);
+// }
+//
+// Person.prototype.lastName = 'Smith';
+
+// Capital "P," because it's a function
+// var john = new Person('John', 1990, 'teacher');
+// Instantiation because these are "instances" of the Person "constructor"
+// var jane = new Person('Jane', 1969, 'designer');
+// var mark = new Person('Mark', 1948, 'retired');
+//
+// john.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+// console.log(john.lastName, jane.lastName, mark.lastName);
+
+/*
+
+The Prototype Chain in the Console
+
+*/
+
 var Person = function (name, yearOfBirth, job) {
       this.name = name;
       this.yearOfBirth = yearOfBirth;
       this.job = job;
 }
 
-Person.prototype.calculateAge = function () {
-      console.log(2018 - this.yearOfBirth);
-}
-
-Person.prototype.lastName = 'Smith';
-
-// Capital "P," because it's a function
 var john = new Person('John', 1990, 'teacher');
-// Instantiation because these are "instances" of the Person "constructor"
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
+console.log(john);
 
-john.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-console.log(john.lastName, jane.lastName, mark.lastName);
+// Console will include all of the properties that we assigned to it. We also have the _proto_, which includes the method that we added.
+console.log(Person.prototype);
+// Built in methods are included in _proto_:Object. Constructor is the prototype that was used to create a new object.
+// Has own property, asks if an object has a property or not.
+console.log(john.hasOwnProperty('job'));
+// Will return true
+console.log(john.hasOwnProperty('lastName'));
+// Will return false, because it's a property on the object Constructor
+console.log(john instanceof Person);
+// Will return true
+
+// Everything IS truly an object
+var x = [2,4,6];
+console.info(x);
+console.log(x.length);
