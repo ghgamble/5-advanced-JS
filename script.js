c0942c52df236ed561c11f793edf501da6e978d6
Grace Gamble <grace.h.gamble@gmail.com>
2018-10-01 15:10:07 -0600

    Call, Bind, Apply

--- script.js
+++ script.js
@@ -260,17 +260,17 @@ Closures
 
 */
 
-function retirement (retirementAge) {
-      var a = ' years left until retirement.';
-      return function (yearOfBirth) {
-            var age = 2018 - yearOfBirth;
-            console.log((retirementAge - age) + a);
-      }
-}
-
-var retirementUS = retirement(66);
-retirementUS(1990);
-retirement(66)(1990);
+// function retirement (retirementAge) {
+//       var a = ' years left until retirement.';
+//       return function (yearOfBirth) {
+//             var age = 2018 - yearOfBirth;
+//             console.log((retirementAge - age) + a);
+//       }
+// }
+//
+// var retirementUS = retirement(66);
+// retirementUS(1990);
+// retirement(66)(1990);
 
 // An inner function always has access to the variables and parameters of its outer function, even after the outer function has returned.
 // Global variables live as long as your application (your window / your web page) lives. Local variables have short lives. They are created when the function is invoked, and deleted when the function is finished.
@@ -278,22 +278,86 @@ retirement(66)(1990);
 
 // Reusable functions, can be used many times with different contexts
 
-var retirementGermany = retirement(65);
-var retirementIceland = retirement(67);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+//
+// retirementGermany(1990);
+// retirementIceland(1990);
+//
+// function interviewQuestion (job) {
+//       return function (name) {
+//             if (job === 'designer') {
+//                   console.log(name + ', can you please explain what UX design is?');
+//             } else if (job === 'teacher') {
+//                   console.log('What subject do you teach, ' + name + '?');
+//             } else {
+//                   console.log('Hello ' + name + ', what do you do?');
+//             }
+//       }
+// }
 
-retirementGermany(1990);
-retirementIceland(1990);
 
-function interviewQuestion (job) {
-      return function (name) {
-            if (job === 'designer') {
-                  console.log(name + ', can you please explain what UX design is?');
-            } else if (job === 'teacher') {
-                  console.log('What subject do you teach, ' + name + '?');
-            } else {
-                  console.log('Hello ' + name + ', what do you do?');
+
+/*
+
+Bind, Call and Apply
+
+*/
+
+var john = {
+      name: 'John',
+      age: 26,
+      job: 'teacher',
+      presentation: function (style, timeOfDay) {
+            if (style === 'formal') {
+                  console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + '. I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+            } else if (style === 'friendly') {
+                  console.log('Hey! What\'s up? I\'m ' + this.name + '. I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
             }
       }
+};
+
+var emily = {
+      name: 'Emily',
+      age: 35,
+      job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+
+// Call method
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// Apply method
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+// Bind method, preset/save arguments
+var johnFriendly = john.presentation.bind(john, 'friendly');
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc (arr, fn) {
+      var arrRes = [];
+      for (var i = 0; i < arr.length; i++) {
+            arrRes.push(fn(arr[i]));
+      }
+      return arrRes;
 }
 
-interviewQuestion ('teacher') ('John');
+function calculateAge (el) {
+      return 2016 - el;
+}
+function isFullAge (limit, el) {
+      return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
